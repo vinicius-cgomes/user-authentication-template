f72b5bdeec18371aff456cdb394833ed9c305056
vinicius-cgomes <viniciuscgomes.98@gmail.com>
2023-08-22 19:17:08 -0300

    User login

--- src/middlewares/userLoginAdapter.js
+++ src/middlewares/userLoginAdapter.js
@@ -5,9 +5,23 @@ async function userLoginAdapter(req, res, next) {
     const { email } = req.params;
     const { password } = req.body;
 
-    // TODO
+    if(!email || !password){
+        return res.status(400).end();
+    }
 
-    req.user = null;
+    const user = await UserController.findByEmail(email);
+
+    if(!user){
+        return res.status(404).end();
+    }
+
+    passwordIsValid = await comparePassword(password, user.dataValues.password);
+
+    if(!passwordIsValid){
+        return res.status(403).end();
+    }
+
+    req.user = user.dataValues;
 
     next();
 };
